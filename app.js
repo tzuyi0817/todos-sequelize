fd32a6089296170af17c4aeed485bc5977954c79
Tzuyi Yang <a0988426059@gmail.com>
2019-05-18 14:43:25 +0800

    add connect-flash messages on login and register

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
 const db = require('./models')
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -23,12 +24,16 @@ app.use(session({
   saveUninitialized: 'false'
 }))
 
+app.use(flash())
+
 app.use(passport.initialize())
 app.use(passport.session())
 require('./config/passport')(passport)
 app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/user.js
+++ routes/user.js
@@ -26,32 +26,48 @@ router.get('/register', (req, res) => {
 //註冊撿查
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body
-  User.findOne({ where: { email: email } }).then(user => {
-    if (user) {
-      console.log('User already exist')
-      res.render('register', { name, email, password, password2 })
-    } else {
-      const newUser = new User({ name, email, password })
 
-      //bcrypt
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err
-          newUser.password = hash
+  let errors = []
 
-          newUser.save().then(user => {
-            res.redirect('/')
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: '所有欄位都是必填' })
+  }
+
+  if (password != password2) {
+    errors.push({ message: '密碼輸入錯誤' })
+  }
+
+  if (errors.length > 0) {
+    res.render('register', { errors, name, email, password, password2 })
+  } else {
+    User.findOne({ where: { email: email } }).then(user => {
+      if (user) {
+        errors.push({ message: '這個信箱已經註冊過了' })
+        res.render('register', { errors, name, email, password, password2 })
+      } else {
+        const newUser = new User({ name, email, password })
+
+        //bcrypt
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
+
+            newUser.save().then(user => {
+              res.redirect('/')
+            })
+              .catch(err => console.log(err))
           })
         })
-      })
-        .catch(err => console.log(err))
-    }
-  })
+      }
+    })
+  }
 })
 
 //登出
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出')
   res.redirect('/users/login')
 })
 
